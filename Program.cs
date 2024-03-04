306d0177c635b06f52f84a8f49dfbb5af8aa721d
anderson oliveira <anderson.oliveira@trestto.com.br>
2024-03-04 17:29:39 -0300

    IncluindoClassPosXadrez

--- Program.cs
+++ Program.cs
@@ -9,23 +9,10 @@ namespace JogoXadrez
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Tabuleiro tab = new Tabuleiro(8, 8);
+           PosicaoXadrez pos = new PosicaoXadrez ('a',1);
 
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+            Console.WriteLine(pos.Toposicao());
 
-
-                Tela.imprimirTabuleiro(tab);
-
-            }
-            catch (Exception ex) {
-            
-                Console.WriteLine(ex.ToString());
-            
-            }
             Console.ReadLine();
 
         }
