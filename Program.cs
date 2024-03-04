794014bf2dea3a09c4e8e2bbbf13d0304ecfe7e4
anderson oliveira <anderson.oliveira@trestto.com.br>
2024-03-04 17:15:55 -0300

    IncluindoExceptionJaExistePecaPosicao

--- Program.cs
+++ Program.cs
@@ -9,14 +9,23 @@ namespace JogoXadrez
     {
         static void Main(string[] args)
         {
-            Tabuleiro tab = new Tabuleiro(8, 8);
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
 
-            Tela.imprimirTabuleiro(tab);
 
+                Tela.imprimirTabuleiro(tab);
+
+            }
+            catch (Exception ex) {
+            
+                Console.WriteLine(ex.ToString());
+            
+            }
             Console.ReadLine();
 
         }
