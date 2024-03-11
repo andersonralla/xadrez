4268b93adb9fd31600d4e0c3defe043a49e29a9b
anderson oliveira <anderson.oliveira@trestto.com.br>
2024-03-11 17:52:43 -0300

    CorrecaoTestesMovimentosPossiveis

--- Tabuleiro/Posicao.cs
+++ Tabuleiro/Posicao.cs
@@ -1,9 +1,8 @@
-﻿
-
-namespace tabuleiro
+﻿namespace tabuleiro
 {
     class Posicao
     {
+
         public int linha { get; set; }
         public int coluna { get; set; }
 
@@ -13,17 +12,17 @@ namespace tabuleiro
             this.coluna = coluna;
         }
 
+        public void definirValores(int linha, int coluna)
+        {
+            this.linha = linha;
+            this.coluna = coluna;
+        }
+
         public override string ToString()
         {
-            return linha + ", " + coluna;
-        }
-
-        public void definirValores(int linha, int coluna) 
-        {
+            return linha
+                + ", "
+                + coluna;
         }
     }
-
-
 }
-
-
