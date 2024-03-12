32ca149bb1b8c1f1de5ed801412455fcc840e4ad
anderson oliveira <anderson.oliveira@trestto.com.br>
2024-03-12 18:39:48 -0300

    ImplementandoRestricoesDeMovimento

--- Program.cs
+++ Program.cs
@@ -15,24 +15,36 @@ namespace JogoXadrez
 
                 while (!partida.terminada)
                 {
+                    try
+                    {
+                        Console.Clear();
+                        Tela.imprimirTabuleiro(partida.tab);
+                        Console.WriteLine();
+                        Console.WriteLine("Turno: " + partida.turno);
+                        Console.WriteLine("Aguardando Jogada " + partida.jogadorAtual);
 
-                    Console.Clear();
-                    Tela.imprimirTabuleiro(partida.tab);
+                        Console.WriteLine();
+                        Console.Write("Origem: ");
+                        Posicao origem = Tela.LerPosicaoXadrez().toPosicao();
+                        partida.validarPosicaoDeOrigem(origem);
 
-                    Console.WriteLine();
-                    Console.Write("Origem: ");
-                    Posicao oringem = Tela.LerPosicaoXadrez().toPosicao();
-
-                    bool[,] posicoesPossiveis = partida.tab.peca(oringem).movimentosPossiveis();
-                    Console.Clear();
-                    Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
+                        bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
+                        Console.Clear();
+                        Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
 
 
-                    Console.WriteLine();
-                    Console.Write("Destino: ");
-                    Posicao destino = Tela.LerPosicaoXadrez().toPosicao();
+                        Console.WriteLine();
+                        Console.Write("Destino: ");
+                        Posicao destino = Tela.LerPosicaoXadrez().toPosicao();
+                        partida.validarPosicaoDeDestino(origem, destino);
 
-                    partida.executaMovimento(oringem, destino);
+                        partida.realizaJogada(origem, destino);
+                    }
+                    catch (TabuleiroException e)
+                    {
+                        Console.WriteLine( e.Message);
+                        Console.ReadLine();
+                    }
                 }
                 Tela.imprimirTabuleiro(partida.tab);
 
--- Tabuleiro/Posicao.cs
+++ Tabuleiro/Posicao.cs
@@ -1,25 +1,20 @@
-﻿namespace tabuleiro
-{
-    class Posicao
-    {
+﻿﻿namespace tabuleiro {
+    class Posicao {
 
         public int linha { get; set; }
         public int coluna { get; set; }
 
-        public Posicao(int linha, int coluna)
-        {
+        public Posicao(int linha, int coluna) {
             this.linha = linha;
             this.coluna = coluna;
         }
 
-        public void definirValores(int linha, int coluna)
-        {
+        public void definirValores(int linha, int coluna) {
             this.linha = linha;
             this.coluna = coluna;
         }
 
-        public override string ToString()
-        {
+        public override string ToString() {
             return linha
                 + ", "
                 + coluna;
